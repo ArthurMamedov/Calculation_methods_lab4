6ea205ff5056706ab4a98db4f83a4365e8a53d40
ArthurMamedovPA-18-1 <43195018+ArthurMamedov@users.noreply.github.com>
2020-06-04 21:45:30 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from modules import *
-
-if __name__ == '__main__':
-    print('Enter the expanded matrix:')
-    matrix = ask_for_matrix()
-
-    print(f'Roots (got using Gauss): {Gauss(list(matrix))}')
-    print('Roots (got using Seidel): {}, iterations: {}'.format(*Seidel(list(matrix), 0.0001)))
